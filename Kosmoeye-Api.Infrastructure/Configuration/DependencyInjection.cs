25da9b83953d94d48d009efac94219ea97b19a19
João Victor <elietejvc@hotmail.com>
2025-06-29 03:43:55 -0300

    feat: configurando service locator e persistencia de dados

---
+++ Kosmoeye-Api.Infrastructure/Configuration/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kosmoeye_Api.Application.UseCases.Users;
+using Kosmoeye_Api.Domain.Interfaces.Repositories;
+using Kosmoeye_Api.Infrastructure.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+namespace Kosmoeye_Api.Infrastructure.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<CreateUserHandler>();
+
+            return services;
+        }
+    }
+}
