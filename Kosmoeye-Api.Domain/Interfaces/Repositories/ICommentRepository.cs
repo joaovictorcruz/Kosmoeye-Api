3caaa4b979f5d3ea7743b64f050f15310e5b6d5c
João Victor <elietejvc@hotmail.com>
2025-07-21 23:59:36 -0300

    feat: adicionando IComment

---
+++ Kosmoeye-Api.Domain/Interfaces/Repositories/ICommentRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kosmoeye_Api.Domain.Interfaces.Repositories
+{
+    public interface ICommentRepository
+    {
+        Task AddAsync(Comment comment);
+        Task<List<Comment>> GetByArtworkIdAsync(Guid artworkId);
+        Task<Comment?> GetByIdAsync(Guid id);
+        Task DeleteAsync(Guid id);
+    }
+}
